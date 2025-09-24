dba347dd19b28b98844c4e52d90f8ebc445ca6cf
cyrmanj <cyrmanj@example.com>
2025-09-24 17:56:20 -0600

    Initial commit

---
+++ lib/demoawspipeline-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep} from 'aws-cdk-lib/pipelines';
+
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class DemoawspipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    // AWS CI_CD Pipeline
+    const democicdpipeline = new CodePipeline(this, 'demopipeline',
+      {
+
+        synth: new ShellStep('Synth', {
+        // Use a connection created using the AWS console to authenticate to GitHub
+        // Other sources are available.
+        input: CodePipelineSource.gitHub('cyrmanj/democicd092425','main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+      }),
+    });
+  }
+}
