27372da0282bac300e6787896b3bfa2a9c3c866f
cyrmanj <cyrmanj@example.com>
2025-09-24 18:40:09 -0600

    "final commit"

--- lib/demoawspipeline-stack.ts
+++ lib/demoawspipeline-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep} from 'aws-cdk-lib/pipelines';
+import {PipelineAppStage} from './demoawspipeline-app-stack';
+import { ManualApprovalStep  } from 'aws-cdk-lib/pipelines';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -16,9 +19,21 @@ export class DemoawspipelineStack extends cdk.Stack {
         synth: new ShellStep('Synth', {
         // Use a connection created using the AWS console to authenticate to GitHub
         // Other sources are available.
-        input: CodePipelineSource.gitHub('cyrmanj/democicd092425','main'),
+        input: CodePipelineSource.gitHub('cyrmanj/democicd092425','main', {
+          authentication: cdk.SecretValue.secretsManager('github-token')
+        }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth'],
       }),
-    });
+      });
+
+      const testingStage = democicdpipeline.addStage(new PipelineAppStage(this, 'test', {
+      env: { account: '742460038467', region: 'us-east-2' }
+    }));
+
+    testingStage.addPost(new ManualApprovalStep('approval'));
+
+    const prodStage = democicdpipeline.addStage(new PipelineAppStage(this, 'prod', {
+      env: { account: '742460038467', region: 'us-east-2' }
+    }));
   }
 }
